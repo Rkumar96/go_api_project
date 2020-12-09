63072f86850abec43932d07b5472be932db7402b
Rahul Kumar <rahul.kumar@powerupcloud.com>
2020-12-09 22:30:58 +0530

    First commit

---
+++ first.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func demo() {
+	fmt.Println("Hello World! This is my first Go program\n")
+	fmt.Println("i am in anither function")
+}
+
+// func main() {
+// 	demo()
+// }
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	//smaple article format
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Discription", Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage endpoint hit")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequest()
+}
